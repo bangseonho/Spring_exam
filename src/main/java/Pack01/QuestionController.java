46ad0d5b98dce688f841426d2600961b4d87fd4d
choyeongwoo <choyeongwoo.s@gmail.com>
2022-03-12 11:36:18 +0900

    develop => cho

--- src/main/java/Pack01/QuestionController.java
+++ src/main/java/Pack01/QuestionController.java
@@ -20,18 +20,12 @@ public class QuestionController implements HttpSessionBindingListener {
 	@Autowired
 	QuestionDAO questionDAO;
 
-<<<<<<< HEAD
-	@RequestMapping("/getQuestion")
-	public String f1(Model model, ResultSet rs) {
-		return "QuestionFormView";
-	}
-=======
+
 //	@RequestMapping("/getQuestion")
 //	public String f1(Model model, ResultSet rs) {
 //
 //		return "QuestionFormView";
 //	}
->>>>>>> develop
 
 	@RequestMapping("/questionform")
 	public String f2(HttpSession session) throws Exception {
--- src/main/java/Question/QuestionDAO.java
+++ src/main/java/Question/QuestionDAO.java
@@ -10,37 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import Pack01.ConnectionDB;
 
 public class QuestionDAO {
-<<<<<<< HEAD
-	@Autowired
-	static
-	ConnectionDB conn1;
-
-	@Autowired
-	QuestionDTO questionDTO;
-
-	@SuppressWarnings("null")
-	public static ResultSet getQuestion() throws Exception {
-		String sql = "SELECT * from question order by rand() limit 5;";
-		try {
-			@SuppressWarnings("static-access")
-			Connection conn = conn1.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			return rs;
-
-		} catch (Exception e) {
-			throw new Exception("QuestionDAO.java occured error");
-		}
-	}
-}
-=======
    @Autowired
    static
    ConnectionDB conn1;
 
-	/*   @Autowired
-	   QuestionDTO questionDTO;*/
-
+   @Autowired
+   QuestionDTO questionDTO;
 
    @SuppressWarnings("null")
    public static ResultSet getQuestion() throws Exception {
@@ -57,4 +32,3 @@ public class QuestionDAO {
       }
    }
 }
->>>>>>> develop
