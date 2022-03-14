2fa1abfeb4a9e00a17cdf764324d07e712b33661
지연 <jiueon6@gmail.com>
2022-03-14 17:56:34 +0900

    저녁맛있게먹어요~

--- src/main/java/Pack01/ResultController.java
+++ src/main/java/Pack01/ResultController.java
@@ -29,62 +29,8 @@ public class ResultController {
 			model.addAttribute("rs3", resultDAO.resultCollectCount(userCode));
 			model.addAttribute("rs1", resultDAO.resultAnswer(userCode));
 		}catch(Exception e){
-			System.out.println("ȸ������ ����");
+			System.out.println("result controller 실패");
 		}
-		
-		
 		return "ResultView";
 	}
-	
-	/*public String form(Model model, ResultDTO result) {
-		System.out.println(123123);
-
-		ResultDAO resultDAO = new ResultDAO(); //select 결과쿼리
-		
-		try {
-			model.addAttribute("rs", resultDAO.resultAnswer());
-		}catch(Exception e){
-			System.out.println("ȸ������ ����");
-		}
-		
-		
-		return "ResultView";
-	}*/
-	
-	
-	/*@Autowired
-	ResultDAO resultDAO;
-	
-	@RequestMapping("/result")
-	public String form(Model model, ResultDTO result) {
-	
-		
-		JSONObject obj1 = new JSONObject();
-		JSONArray jArray = new JSONArray();
-		
-		for(int i=0; i<5; i++){
-		
-			JSONObject obj2 = new JSONObject();
-			
-			obj2.put("name",i);
-			
-			jArray.put(obj2);
-	
-		}
-		
-		
-		obj1.put("data", jArray);
-		
-		String resp = obj1.toString();
-		
-		try {
-			model.addAttribute("dto", resp);
-		} catch (Exception e) {
-			System.out.println("find failed _ controller");
-		}
-		
-	
-		ResultDTO dto = new ResultDTO(result.getCode(), result.getAnswered());
-		return "ResultView";
-	}*/
 }
